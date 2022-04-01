b5c94a334b38971f439bb473abbb81be910b0e02
tidilia <tidiliaaa@gmail.com>
2022-04-01 18:12:30 +0500

    tutorial1

---
+++ cmake-build-debug/CMakeFiles/untitled17.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled17.dir/main.cpp.o"
+  "untitled17"
+  "untitled17.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled17.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

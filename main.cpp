76d2859d8242cca8a5688af04fc968178575167b
tidilia <tidiliaaa@gmail.com>
2022-04-02 16:14:30 +0500

    tutorial2

--- main.cpp
+++ main.cpp
@@ -1,14 +1,44 @@
-#include <GLUT/glut.h>
+//#include "glew.h"
+//#include <GLUT/glut.h>
+#include "/usr/local/Cellar/glew/2.2.0_1/include/GL/glew.h"
+#include "/usr/local/Cellar/freeglut/3.2.2/include/GL/freeglut.h"
+#include <stdio.h>
+#include "/usr/local/Cellar/glm/0.9.9.8/include/glm/vec3.hpp"
+//#include "/usr/local/Cellar/glm/0.9.9.8/include/glm/geometric.hpp"
+
+GLuint VBO;
 
 static void RenderSceneCB()
 {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glDisableVertexAttribArray(0);
+
     glutSwapBuffers();
 }
+
 
 static void InitializeGlutCallbacks()
 {
     glutDisplayFunc(RenderSceneCB);
+}
+
+static void CreateVertexBuffer()
+{
+    glm::vec3 Vertices[3];
+    Vertices[0] = glm::vec3(-1.0f, -1.0f, 0.0f);
+    Vertices[1] = glm::vec3(1.0f, -1.0f, 0.0f);
+    Vertices[2] = glm::vec3(0.0f, 1.0f, 0.0f);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), &Vertices[0], GL_STATIC_DRAW);
 }
 
 
@@ -18,11 +48,20 @@ int main(int argc, char** argv)
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA);
     glutInitWindowSize(1024, 768);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Tutorial 01");
+    glutCreateWindow("Tutorial 03");
 
     InitializeGlutCallbacks();
 
-    glClearColor(1.0f, 0.37f, 0.69f, 0.0f);
+    // Must be done after glut is initialized!
+    GLenum res = glewInit();
+    if (res != GLEW_OK) {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    CreateVertexBuffer();
 
     glutMainLoop();
 

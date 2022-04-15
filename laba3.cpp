8979a3886404d3a1ab94d5fc8b55a4d8ce48791b
tidilia <68690142+tidilia@users.noreply.github.com>
2022-04-15 16:28:37 +0300

    task7

--- laba3.cpp
+++ laba3.cpp
@@ -46,10 +46,10 @@ static void RenderSceneCB()
 
     glm::mat4 World;
 
-    World[0][0] = 1.0f; World[0][1] = 0.0f; World[0][2] = 0.0f; World[0][3] = 0.0f;
-    World[1][0] = 0.0f; World[1][1] = 1.0f; World[1][2] = 0.0f; World[1][3] = 0.0f;
-    World[2][0] = 0.0f; World[2][1] = 0.0f; World[2][2] = 1.0f; World[2][3] = sinf(Scale);
-    World[3][0] = 0.0f; World[3][1] = 0.0f; World[3][2] = 0.0f; World[3][3] = 1.0f;
+    World[0][0] = cosf(Scale); World[0][1] = -sinf(Scale); World[0][2] = 0.0f; World[0][3] = 0.0f;
+    World[1][0] = sinf(Scale); World[1][1] = cosf(Scale);  World[1][2] = 0.0f; World[1][3] = 0.0f;
+    World[2][0] = 0.0f;        World[2][1] = 0.0f;         World[2][2] = 1.0f; World[2][3] = 0.0f;
+    World[3][0] = 0.0f;        World[3][1] = 0.0f;         World[3][2] = 0.0f; World[3][3] = 1.0f;
 
    
     glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World[0][0]); //загрузка данных в uniform переменные шейдера
@@ -152,7 +152,7 @@ int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowSize(768, 1024);
+    glutInitWindowSize(1024,768);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Tutorial 06");
 

8da8c248474a821e4f75281bb92a8c9bf3891b57
tidilia <68690142+tidilia@users.noreply.github.com>
2022-04-15 16:25:48 +0300

    task6

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# graphics
-laboratory works
+static void RenderSceneCB() 
+Это функция рендера. Очищает буфер кадра, создает матрицу преобразований, загружает данные в шейдер, рисует треугольник по кадрам.
+static void InitializeGlutCallbacks() 
+Добавляет функцию рендера, указываеn функцию рендера в качестве ленивой
+static void CreateVertexBuffer()
+Создаем вершины треугольника, добавляем в буфер
+static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
+Gluint ShaderProgaram - переменная для хранения указателя
+const char* pShaderText - текст шейдера в виде массива char
+GLenum ShaderType - тип шейдера
+Функция для добавления шейдера из строк
---
+++ laba3.cpp
@@ -0,0 +1,177 @@
+﻿// laba3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <math.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
+
+GLuint VBO; //переменная для хранения указатель на буфер вершин
+GLuint gWorldLocation; //uniform-переменная для указания на матрицу перемещения
+
+static const char* pVS = "                                                          \n\
+#version 330                                                                        \n\
+                                                                                    \n\
+layout (location = 0) in vec3 Position;                                             \n\
+                                                                                    \n\
+uniform mat4 gWorld;                                                                \n\
+                                                                                    \n\
+void main()                                                                         \n\
+{                                                                                   \n\
+    gl_Position = gWorld * vec4(Position, 1.0);                                     \n\
+}";
+
+static const char* pFS = "                                                          \n\
+#version 330                                                                        \n\
+                                                                                    \n\
+out vec4 FragColor;                                                                 \n\
+                                                                                    \n\
+void main()                                                                         \n\
+{                                                                                   \n\
+    FragColor = vec4(1.0, 0.59, 0.73, 1.0);                                           \n\
+}";
+
+static void RenderSceneCB()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    static float Scale = 0.0f;
+
+    Scale += 0.001f;
+
+    glm::mat4 World;
+
+    World[0][0] = 1.0f; World[0][1] = 0.0f; World[0][2] = 0.0f; World[0][3] = 0.0f;
+    World[1][0] = 0.0f; World[1][1] = 1.0f; World[1][2] = 0.0f; World[1][3] = 0.0f;
+    World[2][0] = 0.0f; World[2][1] = 0.0f; World[2][2] = 1.0f; World[2][3] = sinf(Scale);
+    World[3][0] = 0.0f; World[3][1] = 0.0f; World[3][2] = 0.0f; World[3][3] = 1.0f;
+
+   
+    glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World[0][0]); //загрузка данных в uniform переменные шейдера
+
+    glEnableVertexAttribArray(0); //разрешаем доступ к атрибутам вершин
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); //связываем указатель с назначением
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);//как воспринимать атрибуты
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glDisableVertexAttribArray(0); //закрываем доступ
+
+    glutSwapBuffers();
+}
+
+
+static void InitializeGlutCallbacks()
+{
+    glutDisplayFunc(RenderSceneCB);
+    glutIdleFunc(RenderSceneCB);
+}
+
+static void CreateVertexBuffer() //создаем вершины для треугольника
+{
+    glm::vec3 Vertices[3];
+    Vertices[0] = glm::vec3(-1.0f, -1.0f, 0.0f);
+    Vertices[1] = glm::vec3(1.0f, -1.0f, 0.0f);
+    Vertices[2] = glm::vec3(0.0f, 1.0f, 0.0f);
+
+    glGenBuffers(1, &VBO); //генерация объектов переменного типа(массива)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
+
+static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
+{
+    GLuint ShaderObj = glCreateShader(ShaderType); //создаем указатель на шейдер
+
+    if (ShaderObj == 0) {
+        fprintf(stderr, "Error creating shader type %d\n", ShaderType);
+        exit(0);
+    }
+
+    const GLchar* p[1];
+    p[0] = pShaderText;
+    GLint Lengths[1];
+    Lengths[0] = strlen(pShaderText);
+    glShaderSource(ShaderObj, 1, p, Lengths);
+    glCompileShader(ShaderObj);
+    GLint success;
+    glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        GLchar InfoLog[1024];
+        glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
+        fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
+        exit(1);
+    }
+
+    glAttachShader(ShaderProgram, ShaderObj);
+}
+
+static void CompileShaders()
+{
+    GLuint ShaderProgram = glCreateProgram();
+
+    if (ShaderProgram == 0) {
+        fprintf(stderr, "Error creating shader program\n");
+        exit(1);
+    }
+
+    AddShader(ShaderProgram, pVS, GL_VERTEX_SHADER);
+    AddShader(ShaderProgram, pFS, GL_FRAGMENT_SHADER);
+
+    GLint Success = 0;
+    GLchar ErrorLog[1024] = { 0 };
+
+    glLinkProgram(ShaderProgram);
+    glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
+    if (Success == 0) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glValidateProgram(ShaderProgram);
+    glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
+    if (!Success) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glUseProgram(ShaderProgram);
+
+    gWorldLocation = glGetUniformLocation(ShaderProgram, "gWorld");
+    assert(gWorldLocation != 0xFFFFFFFF);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(768, 1024);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Tutorial 06");
+
+    InitializeGlutCallbacks();
+
+    // Must be done after glut is initialized!
+    GLenum res = glewInit();
+    if (res != GLEW_OK) {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    CreateVertexBuffer();
+
+    CompileShaders();
+
+    glutMainLoop();
+
+    return 0;
+}

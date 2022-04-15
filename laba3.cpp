4b03ccc083f3663e7e25cc4403971c29c5d47c7a
tidilia <68690142+tidilia@users.noreply.github.com>
2022-04-15 16:31:04 +0300

    task8

--- laba3.cpp
+++ laba3.cpp
@@ -46,10 +46,10 @@ static void RenderSceneCB()
 
     glm::mat4 World;
 
-    World[0][0] = cosf(Scale); World[0][1] = -sinf(Scale); World[0][2] = 0.0f; World[0][3] = 0.0f;
-    World[1][0] = sinf(Scale); World[1][1] = cosf(Scale);  World[1][2] = 0.0f; World[1][3] = 0.0f;
-    World[2][0] = 0.0f;        World[2][1] = 0.0f;         World[2][2] = 1.0f; World[2][3] = 0.0f;
-    World[3][0] = 0.0f;        World[3][1] = 0.0f;         World[3][2] = 0.0f; World[3][3] = 1.0f;
+    World[0][0] = sinf(Scale); World[0][1] = 0.0f;         World[0][2] = 0.0f;          World[0][3] = 0.0f;
+    World[1][0] = 0.0f       ; World[1][1] = cosf(Scale);  World[1][2] = 0.0f;          World[1][3] = 0.0f;
+    World[2][0] = 0.0f;        World[2][1] = 0.0f;         World[2][2] = sinf(Scale);   World[2][3] = 0.0f;
+    World[3][0] = 0.0f;        World[3][1] = 0.0f;         World[3][2] = 0.0f;          World[3][3] = 1.0f;
 
    
     glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World[0][0]); //загрузка данных в uniform переменные шейдера
